dd240f07140d499f7f92ac931b894e51bffe1dea
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2023-09-19 00:52:02 +0900

    [ROS] make the logger of ros dirver more quiet

--- src/livox_ros_driver2.cpp
+++ src/livox_ros_driver2.cpp
@@ -39,9 +39,6 @@ using namespace livox_ros;
 #ifdef BUILDING_ROS1
 int main(int argc, char **argv) {
   /** Ros related */
-  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
-    ros::console::notifyLoggerLevelsChanged();
-  }
 
   ros::init(argc, argv, "livox_lidar_publisher");
 
